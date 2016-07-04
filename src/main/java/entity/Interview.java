0d0e5bd63a8985618834ee41e6fe6ea8317d3ca3
Максим Мельников <momelnikov@chel.rusoft.local>
2016-07-04 11:08:58 +0500

    1

--- src/main/java/entity/Interview.java
+++ src/main/java/entity/Interview.java
@@ -1,5 +1,6 @@
 package entity;
 
+import com.j256.ormlite.dao.ForeignCollection;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
