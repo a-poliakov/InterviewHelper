14373e3630833b29a4aa580bb03d04dba3404168
Владислав Марчевский <vdmarchevskiy@chel.rusoft.local>
2016-07-12 11:45:09 +0500

    Версия 1.2.

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -7,6 +7,7 @@ public interface AppConfig {
     String FXML_ADD_INTERVIEW_DLG_URL = "views/add_interview_dlg.fxml";
     String FXML_ADD_INTERVIEWER_DLG_URL = "views/add_interviewer_dlg.fxml";
     String FXML_ADD_CANDIDATE_DLG_URL = "views/add_candidate_dlg.fxml";
+    String FXML_EDIT_CATEGORY_DLG_URL = "views/edit_categories_dlg.fxml";
     // База данных
     String DATABASE_URL = "jdbc:sqlite:InterviewBD.db";
 }
