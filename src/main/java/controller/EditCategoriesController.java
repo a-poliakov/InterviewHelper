1ba0f848c1b9e7fd81deb1b5dfa9df0355399cbb
Максим Мельников <momelnikov@chel.rusoft.local>
2016-07-13 10:26:07 +0500

    Категории фикс

--- src/main/java/controller/EditCategoriesController.java
+++ src/main/java/controller/EditCategoriesController.java
@@ -22,8 +22,6 @@ import javafx.fxml.FXML;
 public class EditCategoriesController {
     private Stage primaryStage;
 
-    private Stage dlgStage;
-
     @FXML
     TextField addEdit;
 
@@ -42,44 +40,46 @@ public class EditCategoriesController {
     TableColumn<Category, String> nameColumn;
 
     public void init(Stage stage) throws SQLException {
-        dlgStage = stage;
+        primaryStage = stage;
     }
 
     @FXML
     private void initialize() throws SQLException {
         nameColumn.setCellValueFactory(new PropertyValueFactory<Category, String>("name"));
-
-        categories.addAll(HelperFactory.getHelper().getCategories());
-        categoryTable.setItems(categories);
-    }
-
-
-    private void viewCategoryToTable() throws SQLException {
-        categoryTable.getItems().removeAll();
-        categories.clear();
         categories.addAll(HelperFactory.getHelper().getCategories());
         categoryTable.setItems(categories);
     }
 
     @FXML
     private void onMouseClickAddButton() throws IOException, SQLException {
-        Category category = HelperFactory.getHelper().getCategoryByName(addEdit.getText());
-        if (category == null) {
-            HelperFactory.getHelper().addCategory(addEdit.getText());
-            viewCategoryToTable();
-            addEdit.clear();
+        if (addEdit.getLength() > 0) {
+            addCategory();
         }
     }
 
     @FXML
     private void onMouseClickDelButton() throws IOException, SQLException {
-        if (categoryTable.getSelectionModel().getSelectedItem() != null) {
-            int selectedCategoryId = categoryTable.getSelectionModel().getSelectedItem().getIdCategory();
-            System.out.println(selectedCategoryId);
-            HelperFactory.getHelper().delCategoryById(selectedCategoryId);
-            viewCategoryToTable();
+        if (categoryTable.getSelectionModel().getSelectedItem() != null ) {
+            removeCategory();
         }
+    }
 
+    private void removeCategory()throws IOException, SQLException {
+        Category category = categoryTable.getSelectionModel().getSelectedItem();
+        int selectedCategoryId = category.getIdCategory();
+        HelperFactory.getHelper().delCategoryById(selectedCategoryId);
+        categories.remove(category);
+    }
+
+    private void addCategory()throws IOException, SQLException {
+        String newCategoryName = addEdit.getText();
+        Category category = HelperFactory.getHelper().getCategoryByName(newCategoryName);
+        if (category == null) {
+            category = HelperFactory.getHelper().addCategory(newCategoryName);
+            addEdit.clear();
+            categories.add(category);
+            categoryTable.setItems(categories);
+        }
     }
 }
 
