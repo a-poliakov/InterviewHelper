21f796bb8ee3ba901797392fe72ecf9a02ef7266
Алексей Афонькин <avafonkin@chel.rusoft.local>
2016-07-15 12:19:19 +0500

    no message

--- src/main/java/config/SystemConfig.java
+++ src/main/java/config/SystemConfig.java
@@ -1,5 +1,21 @@
 package config;
 
+import java.io.File;
+import java.io.IOException;
+
+import static config.AppConfig.Name_File;
+
 public class SystemConfig {
     // TODO: 11.07.2016 Настройка приложения: пока только автозапуск
+    public void run() {
+        File file = new File(System.getProperty("user.dir"));
+        String s;
+        try {
+            s = "reg add HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v Hello /t REG_SZ /d " + file.getPath() + "\\Hello.jar";
+            System.out.println(file.getName());
+            System.out.println(file.getPath());
+            Runtime.getRuntime().exec(s);
+        } catch (Exception ex) {
+        }
+    }
 }
