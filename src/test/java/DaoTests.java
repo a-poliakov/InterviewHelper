77d4309181bcad56b32f4871a60a39fab8d00a3f
Максим Майоров <mpmayorov@chel.rusoft.local>
2016-07-18 11:35:37 +0500

    Валидаторы на имя и дату

--- src/test/java/DaoTests.java
+++ src/test/java/DaoTests.java
@@ -1,3 +1,9 @@
+import config.Validator;
+import jdk.Exported;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * Created by avpolyakov on 15.07.2016.
  */
---
+++ src/test/java/ValidatorTest.java
@@ -0,0 +1,67 @@
+import config.Validator;
+import jdk.Exported;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+/**
+ * Created by mpmayorov on 18.07.2016.
+ */
+public class ValidatorTest {
+    //проверка fio
+    @Test(expected = Exception.class)
+    public void fioNumber() throws Exception {
+        Validator.checkFio("за1за");
+    }
+
+    @Test(expected = Exception.class)
+    public void fioEmptyString() throws Exception {
+        Validator.checkFio("");
+    }
+
+    @Test(expected = Exception.class)
+    public void fioBanChar() throws Exception {
+        Validator.checkFio("1");
+    }
+
+    @Test
+    public void fioSingleChar() throws Exception {
+        Validator.checkFio("a");
+    }
+
+    @Test
+    public void fioDot() throws Exception {
+        Validator.checkFio("...");
+    }
+
+    @Test
+    public void fioTestShortName() throws Exception {
+        Validator.checkFio("Ветров А.Ю");
+    }
+
+    @Test
+    public void fioFullName() throws Exception {
+        Validator.checkFio("Анатолий Петрович Васичкин");
+    }
+
+    //Првоерка даты
+    @Test
+    public void dateFull() throws Exception {
+        Validator.checkDate("05.12.2001");
+    }
+    @Test
+    public void dateShort() throws Exception {
+        Validator.checkDate("5.1.2001");
+    }
+    @Test(expected = Exception.class)
+    public void dateBanChar() throws Exception {
+        Validator.checkDate("1.12.2w01");
+    }
+    @Test(expected = Exception.class)
+    public void dateShortYear() throws Exception {
+        Validator.checkDate("1.12.201");
+    }
+    @Test(expected = Exception.class)
+    public void dateEmpty() throws Exception {
+        Validator.checkDate("");
+    }
+}
